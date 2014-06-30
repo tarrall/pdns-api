82b339e1dc56b1cf0fb26378f850f33a2184155d
Robert Tarrall <robert@protectwise.com>
2014-06-30 15:04:44 -0600

    Switch from mysql to postgres

--- config/adapters.js
+++ config/adapters.js
@@ -1,8 +1,8 @@
 /**
  * Global adapter config
- * 
+ *
  * The `adapters` configuration object lets you create different global "saved settings"
- * that you can mix and match in your models.  The `default` option indicates which 
+ * that you can mix and match in your models.  The `default` option indicates which
  * "saved setting" should be used if a model doesn't have an adapter specified.
  *
  * Keep in mind that options you define directly in your model definitions
@@ -14,7 +14,7 @@
 
 module.exports.adapters = {
 
-  // If you leave the adapter config unspecified 
+  // If you leave the adapter config unspecified
   // in a model definition, 'default' will be used.
   'default': 'disk',
 
@@ -26,14 +26,15 @@ module.exports.adapters = {
 
   // MySQL is the world's most popular relational database.
   // Learn more: http://en.wikipedia.org/wiki/MySQL
-  myLocalMySQLDatabase: {
+  pgDatabase: {
 
-    module: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
+    module: 'sails-postgresql',
+    host: 'localhost',
+    database: 'pdns',
+    user: 'pdns'
+
     // Psst.. You can put your password in config/local.js instead
     // so you don't inadvertently push it up if you're using version control
-    password: 'YOUR_MYSQL_PASSWORD', 
-    database: 'YOUR_MYSQL_DB'
+    // password: 'YOUR_MYSQL_PASSWORD',
   }
 };
