7fc4234f5913f91de8446a7ba30e701fcec23fd3
Robert Tarrall <robert@protectwise.com>
2014-06-30 15:05:03 -0600

    Initial record schema

--- api/models/Record.js
+++ api/models/Record.js
@@ -2,18 +2,40 @@
  * Record
  *
  * @module      :: Model
- * @description :: A short summary of how this model works and what it represents.
+ * @description :: A DNS record, e.g. an A or a PTR record.
+ *                 Note one pdns-specific behavior: in order to support DNSSEC, ordername and auth
+ *                 columns need to be filled in correctly.
  * @docs		:: http://sailsjs.org/#!documentation/models
  */
 
 module.exports = {
 
   attributes: {
-  	
-  	/* e.g.
-  	nickname: 'string'
-  	*/
-    
+    domain_id: 'int',
+    name: {
+      type: 'string',
+      required: true,
+      maxLength: 255
+    },
+    type:  {
+      type: 'string',
+      required: true,
+      maxLength: 10
+    },
+    content:  {
+      type: 'string',
+      required: true,
+      maxLength: 255
+    },
+    ttl: 'int',
+    prio: 'int',
+    change_date: 'int',
+    ordername:  {
+      type: 'string',
+      required: true,
+      maxLength: 255
+    },
+    auth: 'boolean'
   }
 
 };
